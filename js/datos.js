7edce1604d258426e4d691249b0e9db0dc29ad11
FacundoCufre <99839627+FacundoCufre@users.noreply.github.com>
2022-05-01 01:34:22 -0300

    preentrega

--- js/datos.js
+++ js/datos.js
@@ -1,10 +1,8 @@
+const urlCategorias = `js/categorias.json`
 
-const categorias = [
-    {id: 1, nombre: "CASA"},
-    {id: 2, nombre: "DEPARTAMENTO"},
-    {id: 3, nombre: "CABAÑA"},
-    {id: 4, nombre: "PENT-HOUSE"},
-]
+fetch(urlCategorias)
+.then((response)=>response.json())
+.then((datos)=>mostrarMenu(datos))
 
 const transacciones = [
     {id: 1, nombre: "ALQUILER"},
@@ -13,7 +11,7 @@ const transacciones = [
 ]
 
 class Propiedades {
-    constructor(nombre, tipo, precio, valor, accion, transaccion, id) {
+    constructor(nombre, tipo, precio, valor, accion, transaccion, id, ambs, habits, baños, balcon, patio, garaje) {
         this.nombre = nombre.toUpperCase()
         this.tipo = tipo
         this.precio = precio
@@ -21,31 +19,37 @@ class Propiedades {
         this.accion = accion
         this.transaccion = transaccion.toUpperCase()
         this.id = id
+        this.ambs = ambs
+        this.habits = habits
+        this.baños = baños
+        this.balcon = balcon.toUpperCase()
+        this.patio = patio.toUpperCase()
+        this.garaje = garaje.toUpperCase()
     }
 }
 
 const propiedades = [];
 
-propiedades.push(new Propiedades("casa dos pisos", 1, "$100.000 mensual", 100000, 1, "alquiler", 0))
-propiedades.push(new Propiedades("casa frente al mar", 1, "$7.000 diario", 7000, 3, "alquiler vacacional", 1))
-propiedades.push(new Propiedades("casa pequeña", 1, "$60.000 mensual", 60000, 1, "alquiler", 2))
-propiedades.push(new Propiedades("casa de tres pisos", 1, "$40.000.000", 40000000, 2, "compra", 3))
-propiedades.push(new Propiedades("casa de playa", 1, "$6000 diario", 6000, 3, "alquiler vacional", 4))
-propiedades.push(new Propiedades("casa de vecindario", 1, "$20.000.000", 200000000, 2, "compra", 5))
-propiedades.push(new Propiedades("casa antigüa", 1, "$28.000.000", 28000000, 2, "compra", 6))
+propiedades.push(new Propiedades("casa dos pisos", 1, "$100.000 mensual", 100000, 1, "alquiler", 0, 6, 5, 3, "si", "si", "si"))
+propiedades.push(new Propiedades("casa frente al mar", 1, "$7.000 diario", 7000, 3, "alquiler vacacional", 1, 6, 4, 2, "si", "si", "si"))
+propiedades.push(new Propiedades("casa pequeña", 1, "$60.000 mensual", 60000, 1, "alquiler", 2, 3, 2, 1, "si", "no", "si"))
+propiedades.push(new Propiedades("casa de tres pisos", 1, "$40.000.000", 40000000, 2, "compra", 3, 7, 6, 7, "si", "si", "si"))
+propiedades.push(new Propiedades("casa de playa", 1, "$6000 diario", 6000, 3, "alquiler vacional", 4, 4, 4, 3, "si", "no", "no"))
+propiedades.push(new Propiedades("casa de vecindario", 1, "$20.000.000", 200000000, 2, "compra", 5, 3, 3, 2, "si", "si", "si"))
+propiedades.push(new Propiedades("casa antigüa", 1, "$28.000.000", 28000000, 2, "compra", 6, 6, 7, 5, "si", "si", "no"))
 
-propiedades.push(new Propiedades("departamento monoambiente", 2, "25.000 mensual", 25000, 1, "alquiler", 7))
-propiedades.push(new Propiedades("departamento 3 ambientes", 2, "$30.000.000", 30000000, 2, "compra", 8))
-propiedades.push(new Propiedades("departamento de estudiantes", 2, "$20.000 mensual", 20000, 1, "alquiler", 9))
-propiedades.push(new Propiedades("departamento loft moderno", 2, "$28.000.000", 28000000, 2, "compra", 10))
-propiedades.push(new Propiedades("departamento loft industrial", 2, "$35.000 mensual", 35000, 1, "alquiler", 11))
+propiedades.push(new Propiedades("departamento monoambiente", 2, "25.000 mensual", 25000, 1, "alquiler", 7, 1, 1, 1, "si", "no", "no"))
+propiedades.push(new Propiedades("departamento 3 ambientes", 2, "$30.000.000", 30000000, 2, "compra", 8, 3, 2, 1, "si", "no", "si"))
+propiedades.push(new Propiedades("departamento de estudiantes", 2, "$20.000 mensual", 20000, 1, "alquiler", 9, 1, 1, 1, "si", "no", "no"))
+propiedades.push(new Propiedades("departamento loft moderno", 2, "$28.000.000", 28000000, 2, "compra", 10, 3, 3, 2, "si", "no", "si"))
+propiedades.push(new Propiedades("departamento loft industrial", 2, "$35.000 mensual", 35000, 1, "alquiler", 11, 3, 3, 2, "si", "no", "si"))
 
-propiedades.push(new Propiedades("cabaña en el bosque", 3, "$40.000 mensual", 40000, 1, "alquiler", 12))
-propiedades.push(new Propiedades("cabaña en la nieve", 3, "$30.000.000", 30000000, 2, "compra", 13))
-propiedades.push(new Propiedades("cabaña en lago", 3, "$10.000 diario", 10000, 3, "alquiler vacacional", 14))
+propiedades.push(new Propiedades("cabaña en el bosque", 3, "$40.000 mensual", 40000, 1, "alquiler", 12, 3, 2, 2, "no", "no", "si"))
+propiedades.push(new Propiedades("cabaña en la nieve", 3, "$30.000.000", 30000000, 2, "compra", 13, 2, 3, 2, "no", "no", "si"))
+propiedades.push(new Propiedades("cabaña en lago", 3, "$10.000 diario", 10000, 3, "alquiler vacacional", 14, 2, 2, 1, "si", "no", "si"))
 
-propiedades.push(new Propiedades("pent-house abierto", 4, "$130.000 mensual", 130000, 1, "alquiler", 15))
-propiedades.push(new Propiedades("pent-house dos pisos", 4, "$180.000 mensual", 180000, 1, "alquiler", 16))
+propiedades.push(new Propiedades("pent-house abierto", 4, "$130.000 mensual", 130000, 1, "alquiler", 15, 3, 4, 3, "no", "no", "si"))
+propiedades.push(new Propiedades("pent-house dos pisos", 4, "$180.000 mensual", 180000, 1, "alquiler", 16, 4, 4, 3, "no", "no", "si"))
 
 
 class Favs{
@@ -73,5 +77,30 @@ class Favs{
 
     guardar(){
         localStorage.setItem("MIS_FAVS",JSON.stringify(this.propiedades));
+    }
+}
+
+
+class Comparaciones{
+
+    constructor(propiedades){
+        this.propiedades=propiedades;
+    }
+
+    addComp(propiedad){   
+        let mapped= this.propiedades.map(element=>element.propiedad);
+        
+        let comps = mapped.find(element=>element.id===propiedad.id);
+
+        !comps ? this.propiedades.push({cantidad:1, propiedad}) : aComparar--
+    }
+    elimComp(propiedad){   
+        let mapped= this.propiedades.map(element=>element.propiedad);
+        
+        let comps = mapped.find(element=>element.id===propiedad.id);
+        let indexed = mapped.map(element=>element.id)
+        let index = indexed.indexOf(propiedad.id)
+
+        comps && this.propiedades.splice(index,1) && aComparar--
     }
 }
--- js/funciones.js
+++ js/funciones.js
@@ -2,6 +2,7 @@ function mostrarPropConFav (x, y){
     
     x.innerHTML =  ` <div class="propiedad__div">
                             <div class="boton--fav" onclick="agregarFav(${y.id})"><i class="fa-solid fa-star"></i></div>
+                            <div class="boton--comp" onclick="evaluarCompar(${y.id})"><i class="fa-solid fa-scale-balanced"></i></i></div>
                             <div class="imagen">
                                 <img width=100% src="img/${y.id}.jpg" alt="${y.nombre}">
                             </div>
@@ -31,6 +32,40 @@ function mostrarPropSinFav (x, y){
                         </div>`
 }
 
+function mostrarPropComparada(x, y){
+    x.innerHTML =  ` 
+                            
+                            <div class="imagen">
+                                <div class="boton--fav" onclick="agregarFav(${y.propiedad.id})"><i class="fa-solid fa-star"></i></div>
+                                <div class="boton--elim2" onclick="eliminarComp(${y.propiedad.id})"><i class="fa-solid fa-trash-can"></i></div>
+                                <img width=100% src="img/${y.propiedad.id}.jpg" alt="${y.propiedad.nombre}">
+                            </div>
+                            <div>
+                                <span>${y.propiedad.nombre}</span>
+                            </div>
+                            <div>
+                                <span>${y.propiedad.precio}</span>
+                            </div>
+                            <div id="ambs--${y.propiedad.id}">
+                                <span>Ambientes: ${y.propiedad.ambs}</span>
+                            </div>
+                            <div id="habits--${y.propiedad.id}">
+                                <span>Habitaciones: ${y.propiedad.habits}</span>
+                            </div>
+                            <div id="baños--${y.propiedad.id}">
+                                <span>Baños: ${y.propiedad.baños}</span>
+                            </div>
+                            <div>
+                                <span>Balcón: ${y.propiedad.balcon}</span>
+                            </div>
+                            <div>
+                                <span>Patio: ${y.propiedad.patio}</span>
+                            </div>
+                            <div>
+                                <span>Garaje: ${y.propiedad.garaje}</span>
+                            </div>`
+}
+
 const conteiner = document.getElementById("conteiner")
 
 
@@ -39,27 +74,66 @@ const conteiner = document.getElementById("conteiner")
 function mostrarPremenu(){
     const todos = document.getElementById("todos")
     const favs = document.getElementById("favs")
+    const inicio = document.getElementById("inicio")
+    const compar = document.getElementById("compar")
+    const info = document.getElementById("info")
+    const comparar = document.getElementById("comparar")
     const filtros = document.getElementById("filtros")
     const todasProps = document.getElementById("todas--props")
     const misFavs = document.getElementById("mis--favs")
-    const titulo1 = document.getElementById("titulo1")
+
+    inicio.addEventListener("click", ()=>{
+        info.classList.add("show2")
+
+        filtros.classList.remove("show")
+
+        misFavs.classList.remove("show2")
+
+        todasProps.classList.remove("show2")
+
+        comparar.classList.remove("show2")
+
+        
+    })
 
     todos.addEventListener("click", ()=>{
+        info.classList.remove("show2")
+
         filtros.classList.add("show")
 
         misFavs.classList.remove("show2")
 
         todasProps.classList.add("show2")
 
+        comparar.classList.remove("show2")
+
     })
 
     favs.addEventListener("click", ()=>{
+        info.classList.remove("show2")
+
         filtros.classList.remove("show")
 
         todasProps.classList.remove("show2")
 
         misFavs.classList.add("show2")
 
+        comparar.classList.remove("show2")
+
+    })
+
+    compar.addEventListener("click", ()=>{
+        info.classList.remove("show2")
+
+        filtros.classList.remove("show")
+
+        misFavs.classList.remove("show2")
+
+        todasProps.classList.remove("show2")
+
+        comparar.classList.add("show2")
+
+        
     })
 }
 
@@ -67,16 +141,14 @@ function mostrarPremenu(){
 /* BOTONES DEL MENU                --------------------------------------  */
 
 function mostrarFiltros(){
-    mostrarMenu()
     mostrarMenu2()
     mostrarMenu3()
-    
 }
 
         /* MENU TIPOS DE PROPIEDADES */
 
-function mostrarMenu(){
-    categorias.forEach((categoria)=>{
+function mostrarMenu(x){
+    x.forEach((categoria)=>{
         const {id, nombre} = categoria
         const filtro = document.getElementById("propiedades")
         const myBtn = document.createElement("div");
@@ -121,8 +193,8 @@ function mostrarMenu3(){
             myBtn.setAttribute("id", `boton--precio`)
             myBtn.innerHTML=`<span>FILTRAR</span>`
             myBtn.addEventListener("click", ()=>{
-                !isNaN(precio) ? mostrarPropiedadesFiltradas3(precio.value) : noValido()
-                precio < 6000 && noValido()
+                !isNaN(precio.value) ? mostrarPropiedadesFiltradas3(precio.value) : noValido()
+                precio.value < 6000 && noValido()
             })
 
         filtro.appendChild(myBtn);
@@ -138,8 +210,6 @@ function mostrarMenu3(){
         }).showToast()
     }
 }
-
-
 
 /* MOSTRAR TODAS LAS PROPIEDADES SIN FILTRAR                 --------------------------------------  */
 
@@ -283,6 +353,103 @@ function eliminarFav(x){
         style: {
             background: "rgb(97, 30, 30)",
             color: "white",
+        }
+    }).showToast()
+}
+
+/* AGREGAR COMPARACION                                   --------------------------------------  */
+
+comps = new Comparaciones([]);
+let aComparar = 0
+
+function evaluarCompar(x){
+    aComparar < 2 ? agregarCompar(x) : Toastify({
+        text: "Solo puedes comparar 2 propiedades a la vez",
+        duration: 3000,
+        gravity: "top",
+        style: {
+            background: "blue",
+            color: "white",
+        }
+    }).showToast()
+}
+
+function agregarCompar(x){
+    let props = propiedades.map(el=>el.id)
+    let index = props.find(el=>el===x)
+    let fav = propiedades[index]
+    comps.addComp(fav);
+    actualizarComps()
+    aComparar++
+    aComparar === 2 && compara()
+    Toastify({
+        text: "Propiedad agradada a comparación",
+        duration: 3000,
+        gravity: "bottom",
+        style: {
+            background: "#364bc2",
+            color: "black",
+        }
+    }).showToast()
+}
+
+function actualizarComps(){
+    let contenedor3 = document.getElementById("conteinerCompar");
+    contenedor3.innerHTML=``;
+    let comparacion = comps.propiedades;
+
+    comparacion.forEach(element => {
+        const {propiedad:{id}} = element
+        const prop = document.createElement(`div`)
+        prop.setAttribute("class", "propiedadComparada__div")
+        prop.setAttribute("id", `comparada--${id}`)
+
+        mostrarPropComparada(prop, element)
+
+        contenedor3.appendChild(prop);
+    });
+}
+
+function compara(){
+    const ambs1 = comps.propiedades[0].propiedad.ambs
+    const habits1 = comps.propiedades[0].propiedad.habits
+    const baños1 = comps.propiedades[0].propiedad.baños
+
+    const ambsid1 = document.getElementById(`ambs--${comps.propiedades[0].propiedad.id}`)
+    const habitsid1 = document.getElementById(`habits--${comps.propiedades[0].propiedad.id}`)
+    const bañoid1 = document.getElementById(`baños--${comps.propiedades[0].propiedad.id}`)
+
+    const ambs2 = comps.propiedades[1].propiedad.ambs
+    const habits2 = comps.propiedades[1].propiedad.habits
+    const baños2 = comps.propiedades[1].propiedad.baños
+
+    const ambsid2 = document.getElementById(`ambs--${comps.propiedades[1].propiedad.id}`)
+    const habitsid2 = document.getElementById(`habits--${comps.propiedades[1].propiedad.id}`)
+    const bañoid2 = document.getElementById(`baños--${comps.propiedades[1].propiedad.id}`)
+
+    ambs1 >= ambs2 ? ambsid1.setAttribute("style", "background-color: green;") : ambsid1.setAttribute("style", "background-color: red;")
+    ambs2 >= ambs1 ? ambsid2.setAttribute("style", "background-color: green;") : ambsid2.setAttribute("style", "background-color: red;")
+
+    habits1 >= habits2 ? habitsid1.setAttribute("style", "background-color: green;") : habitsid1.setAttribute("style", "background-color: red;")
+    habits2 >= habits1 ? habitsid2.setAttribute("style", "background-color: green;") : habitsid2.setAttribute("style", "background-color: red;")
+
+    baños1 >= baños2 ? bañoid1.setAttribute("style", "background-color: green;") : bañoid1.setAttribute("style", "background-color: red;")
+    baños2 >= baños1 ? bañoid2.setAttribute("style", "background-color: green;") : bañoid2.setAttribute("style", "background-color: red;")
+}
+
+function eliminarComp(x){
+    let props = propiedades.map(el=>el.id)
+    let index = props.find(el=>el===x)
+    let comp = propiedades[index]
+    comps.elimComp(comp);
+    actualizarComps()
+    Toastify({
+        text: "Propiedad eliminada de comparación",
+        duration: 3000,
+        gravity: "bottom",
+        style: {
+            background: "#364bc2",
+            color: "black",
         }
     }).showToast()
 }
